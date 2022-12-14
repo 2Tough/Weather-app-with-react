4eeb8625d13f87e5a316b0ee836cae86efd7071b
Polaski <101113655+2Tough@users.noreply.github.com>
2022-12-14 15:52:13 +0300

    Removed API keys.

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'ab3f4e3536msh3397133bf118fedp1eae29jsnd7f698c4186f',
+		'X-RapidAPI-Key': ************************************',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
@@ -9,5 +9,5 @@ export const geoApiOptions = {
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
-export const WEATHER_API_KEY = "fbdf4d9f0d7729c5d3ecad8b7ab66593"
+export const WEATHER_API_KEY = "***********************************"
 
